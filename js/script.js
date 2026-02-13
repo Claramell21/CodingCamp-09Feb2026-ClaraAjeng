732c1d2990a2784e6d342f2c09274efd84e21398
Claramell21 <clarapungus1@gmail.com>
2026-02-13 21:32:23 +0700

    javascript form
    validate  first name, last name, & email

--- js/script.js
+++ js/script.js
@@ -15,4 +15,58 @@ function welcomeMessage() {
     document.getElementById('welcome-speech').innerHTML = `Hello, ${name}! Welcome to Clara Company.`;
 }
 
-function validateForm() { }
+var firstnameError = document.getElementById('firstname-error');
+var lastnameError = document.getElementById('lastname-error');
+var emailError = document.getElementById('email-error');
+var messageError = document.getElementById('message-error');
+var submitError = document.getElementById('submit-error');
+
+function validateFirstName(){
+	var name = document.getElementById('firstname').value;
+        if(name.length == 0){
+        firstnameError.innerHTML = 'First name is not filled yet!';
+        return false;
+        }
+        firstnameError.innerHTML ='valid';
+        return true;
+}
+
+function validateLastName(){
+	var name = document.getElementById('lastname').value;
+        if(name.length == 0){
+        lastnameError.innerHTML = 'Last name is not filled yet!';
+        return false;
+        }
+        lastnameError.innerHTML ='valid';
+        return true;
+}
+
+function validateEmail(){
+	var email = document.getElementById('email').value;
+        if(email.length == 0){
+        emailError.innerHTML = 'Email is not filled yet!';
+        return false;
+        }
+        if(!email.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)){
+        emailError.innerHTML = 'Email Invalid'
+        return false;
+        }
+        emailError.innerHTML ='valid';
+        return true;
+}
+
+
+
+
+
+
+function validateForm() { 
+
+
+
+
+
+
+
+    
+}
